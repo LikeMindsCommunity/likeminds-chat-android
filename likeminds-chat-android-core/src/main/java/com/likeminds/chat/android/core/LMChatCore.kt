8fd4b9a3f8c95702f847c28b6f1db7567e88f861
Ishaan Jain <ishaan.jain@likeminds.community>
2024-09-20 12:39:22 +0530

    added LMChatCore class

---
+++ likeminds-chat-android-core/src/main/java/com/likeminds/chat/android/core/LMChatCore.kt
@@ -0,0 +1,15 @@
+package com.likeminds.chat.android.core
+
+import android.app.Application
+
+object LMChatCore {
+
+    fun setup(
+        application: Application,
+        domain: String?,
+        enablePushNotification:Boolean = false,
+        deviceId:String? = null,
+    ) {
+
+    }
+}
